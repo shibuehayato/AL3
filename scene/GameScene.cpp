3c059e0a12e488a340720a8b0c2dc4275b705bc4
k022g0085@g.neec.ac.jp <k022g0085@g.neec.ac.jp>
2023-04-19 17:07:45 +0900

    途中まで

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -4,7 +4,10 @@
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete sprite_;
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
@@ -12,9 +15,47 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("sample.png");
+
+	// スプライトの生成
+	sprite_ = Sprite::Create(textureHandle_, { 100,50 });
+
+	// 3Dモデルの生成
+	model_ = Model::Create();
+
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	// ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+
+	// サウンドデータの読み込み
+	soundDateHandle_ = audio_->LoadWave("fanfare.wav");
+
+	// 音声再生
+	audio_->PlayWave(soundDateHandle_);
+	// 音声再生
+	voiceHandle_ = audio_->PlayWave(soundDateHandle_, true);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+// スプライトの今の座標を取得
+	Vector2 position = sprite_->GetPosition();
+	// 座標を{ 2,1 }移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+	// 移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+
+	// スペースキーを押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+		// 音声停止
+		audio_->StopWave(voiceHandle_);
+	}
+
+
+}
 
 void GameScene::Draw() {
 
@@ -28,6 +69,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
+	sprite_->Draw();
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
@@ -42,6 +84,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
