39b2802c68f293111915d9f91396f023f0a8987b
k022g0085@g.neec.ac.jp <k022g0085@g.neec.ac.jp>
2023-06-16 16:07:15 +0900

    02_07a完了

--- Enemy.cpp
+++ Enemy.cpp
@@ -13,33 +13,63 @@ void Enemy::Initialize(Model* model, const Vector3& position)
 
 	worldTransform_.Initialize();
 
+	move_ = {0, 0, 0};
+	kCharacterSpeed_ = 0.002f;
+
+	static_cast<size_t>(phase_);
+
 	// 引数で初期座標をリセット
 	worldTransform_.translation_ = position;
 
 }
 
+// フェーズの関数テーブル
+void (Enemy::*Enemy::spPhaseTable[])() = {
+	&Enemy::Approach,
+	&Enemy::Leave
+};
+
+void Enemy::Approach()
+{
+	move_.z -= kCharacterSpeed_;
+
+		if (worldTransform_.translation_.z < 0.0f) {
+		(this->*spPhaseTable[1])();
+		}
+
+}
+
+void Enemy::Leave()
+{
+	    move_.x -= kCharacterSpeed_;
+	    move_.y += kCharacterSpeed_;
+}
+
 void Enemy::Update()
 { 
 	// 敵の移動ベクトル
-	Vector3 move = {0, 0, 0};
+	//Vector3 move = {0, 0, 0};
 	// 敵の移動速さ
-	const float kCharacterSpeed = 0.2f;
-	switch (phase_) { 
-	case Phase::approach:
-	default:
-		move.z -= kCharacterSpeed;
-		// 規定の位置に到達したら削除
-		if (worldTransform_.translation_.z < 0.0f) {
-			phase_ = Phase::leave;
-		}
-		break;
-	case Phase::leave:
-		move.x -= kCharacterSpeed;
-		move.y += kCharacterSpeed;
-		break;
-	}
+	//const float kCharacterSpeed = 0.2f;
+	
+	//switch (phase_) { 
+	//case Phase::approach:
+	//default:
+	//	move.z -= kCharacterSpeed;
+	//	// 規定の位置に到達したら削除
+	//	if (worldTransform_.translation_.z < 0.0f) {
+	//		phase_ = Phase::leave;
+	//	}
+	//	break;
+	//case Phase::leave:
+	//	move.x -= kCharacterSpeed;
+	//	move.y += kCharacterSpeed;
+	//	break;
+	//}
+	
+	(this->* spPhaseTable[0])();
 	// 移動ベクトルを加算
-	worldTransform_.translation_ = VectorAdd(worldTransform_.translation_, move);
+	worldTransform_.translation_ = VectorAdd(worldTransform_.translation_, move_);
 	worldTransform_.UpdateMatrix();
 }
 
--- Enemy.h
+++ Enemy.h
@@ -8,14 +8,21 @@ enum class Phase {
 
 class Enemy {
 public:
+	// 自作メンバ関数
+	void Approach();
+	void Leave();
 	void Initialize(Model* model, const Vector3& position);
 	void Update();
 	void Draw(const ViewProjection& viewProjection);
 
 private:
+	// メンバ関数ポインタのテーブル
+	static void (Enemy::*spPhaseTable[])();
 	WorldTransform worldTransform_;
 	Model* model_ = nullptr;
 	uint32_t textureHandle_ = 0u;
 	Vector3 velocity_;
+	Vector3 move_;
+	float kCharacterSpeed_;
 	Phase phase_ = Phase::approach;
 };
